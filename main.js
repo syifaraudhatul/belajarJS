ecb3f6c02b68f691118f0ace2707d0565ae75274
syifaraudhatul <syifara.26@upi.edu>
2023-10-16 01:21:56 +0700

    sementara, sudah aga mual

--- main.js
+++ main.js
@@ -140,13 +140,54 @@
 // console.log(x==6||y==5)
 // console.log(!(x==y))
 
-let p = document.querySelector("p")
-let button = document.querySelector("button")
-let input = document.querySelector("input")
+// let p = document.querySelector("p")
+// let button = document.querySelector("button")
+// let input = document.querySelector("input")
 
-button.addEventListener('click', function(){
-    let isi = input.value
-    console.log(isi)
-    // p.innerHTML = isi
-    p.innerText = isi
-})
+// button.addEventListener('click', function(){
+//     let isi = input.value
+//     console.log(isi)
+//     p.innerHTML = isi
+//     // p.innerText = isi
+// })
+
+const inputBox = document.getElementById("inbox-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("Masukan list dahulu");
+    } else {
+        let li = document.createElement("li");
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.className = "checkbox";
+        li.appendChild(checkbox);
+        let taskText = document.createTextNode(inputBox.value);
+        li.appendChild(taskText);
+        
+        let removeButton = document.createElement("button");
+        removeButton.textContent = "X";
+        removeButton.className = "remove-button";
+        removeButton.style.marginLeft = "10px"
+        li.appendChild(removeButton);
+        listContainer.appendChild(li);
+        
+        inputBox.value = "";
+        li.style.listStyleType = "none";
+        
+        removeButton.addEventListener("click", function () {
+            li.remove();
+        });
+    }
+}
+
+listContainer.addEventListener("change", function (e) {
+    if (e.target.tagName === "INPUT" && e.target.type === "checkbox") {
+        if (e.target.checked) {
+            e.target.parentElement.style.textDecoration = "line-through";
+        } else {
+            e.target.parentElement.style.textDecoration = "none";
+        }
+    }
+}); 
